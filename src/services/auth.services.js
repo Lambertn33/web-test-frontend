2e49a99a4521eb45632e597a29502ed726c75f1a
Lambertn33 <lambertgithub@gmail.com>
2023-05-09 18:38:03 +0200

    feat: add environnment variables

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,23 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 import jwt_decode from "jwt-decode";
-
 // change url in large project
-const authUrl = 'http://localhost:5000/api/login';
+const authUrl = `http://localhost:${process.env.REACT_APP_BACKEND_PORT}/api/login`;
 
 export async function login(email, password) {
-    let response = await axios.post(authUrl, { email, password });
-    let { token } = response.data;
-    let { authenticatedUser: currentUser }= jwt_decode(token);
-    localStorage.setItem('currentUser', JSON.stringify(currentUser));
-}
-
-export function getCurrentUser() {
-    let loggedInUser = localStorage.getItem('currentUser')
-    loggedInUser = JSON.parse(loggedInUser);
-    return loggedInUser;
+  const response = await axios.post(authUrl, { email, password });
+  const { token } = response.data;
+  const { authenticatedUser: currentUser } = jwt_decode(token);
+  localStorage.setItem("currentUser", JSON.stringify(currentUser));
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    login
-}
+  login,
+};
