16890c81187f01feb424f5f1f6c6c25fa55cc100
Lambertn33 <lambertgithub@gmail.com>
2023-05-09 12:35:39 +0200

    feat: install mdbootstrap

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { MDBBtn } from "mdb-react-ui-kit";
+
+function App() {
+  return (
+    <div className="app">
+      <MDBBtn>Button</MDBBtn>
+    </div>
+  );
+}
+
+export default App;
